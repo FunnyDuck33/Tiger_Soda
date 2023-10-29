d5e155a78145d1e9086e0b73ad05f326675da3b8
Alexander Akimov <funnyduck@yandex-team.ru>
2023-10-29 17:08:23 +0100

    правки путей для статики

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const nextConfig = {
     output: 'export',
+    assetPrefix: './',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import "src/app/[locale]/styles/variables.scss";`
