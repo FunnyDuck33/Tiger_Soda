15e57a63db8d235db4c8986a4f653f6ae391dda7
Alexander Akimov <funnyduck@yandex-team.ru>
2024-03-07 21:53:36 +0300

    шеры

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,30 @@
 import {ReactNode} from 'react';
+import type {Metadata} from "next";
+
+import index_en from '@/../public/assets/shares/index_en.png';
+import index_es from '@/../public/assets/shares/index_es.png';
 
 type Props = {
     children: ReactNode;
 };
+
+export function generateMetadata({params}: {params: {locale: string}}): Metadata {
+    const locale = params.locale as keyof typeof localeMap;
+
+    const localeMap = {
+        'en': index_en.src,
+        'es': index_es.src
+    }
+
+    return {
+        title: 'Tiger Soda',
+        description: 'Ethical digital agency',
+        metadataBase: new URL('https://funnyduck33.github.io'),
+        openGraph: {
+            images: localeMap[locale],
+        },
+    }
+}
 
 export default function RootLayout({children}: Props) {
     return children;
